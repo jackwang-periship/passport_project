c6ca92d7ca8ecfb89e0251c3fc63bf5ea77c4192
Calvin Ling <calvinling8@gmail.com>
2020-07-10 09:31:36 -0400

    Add checkbox for selecting all contracts

--- wiawdp/tables.py
+++ wiawdp/tables.py
@@ -3,7 +3,8 @@ import django_tables2 as tables
 
 
 class ContractTable(tables.Table):
-    select = tables.TemplateColumn(template_name='wiawdp/contract_table_checkbox_select.html', verbose_name='')
+    row_pks = tables.CheckBoxColumn({'th__input': {'id': 'checkbox-all-toggle'}, 'td__input': {'form': 'delete-form'}},
+                                    accessor='pk')
     pk = tables.Column(verbose_name='RecId')
     client = tables.Column()
     actions = tables.TemplateColumn(template_name="wiawdp/contract_table_actions.html", orderable=False)
@@ -14,7 +15,7 @@ class ContractTable(tables.Table):
     class Meta:
         model = Contract
         template_name = 'django_tables2/bootstrap.html'
-        fields = ('select', 'pk', 'client', 'workforce', 'end_date', 'performance')
+        fields = ('row_pks', 'pk', 'client', 'workforce', 'end_date', 'performance')
 
 
 class WIAWDPTable(tables.Table):
