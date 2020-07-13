bec392db68e53c177bd83a57922b2e6f0ff28bc6
Calvin Ling <calvinling8@gmail.com>
2020-07-13 03:44:47 -0400

    Create a custom widget using Inputmask

--- wiawdp/forms.py
+++ wiawdp/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from phonenumber_field.formfields import PhoneNumberField
 import wiawdp.widgets as widgets
 
+
 class ViewReportForm(forms.Form):
     start_date = forms.DateField(label="From", widget=widgets.DatePickerWidget())
     end_date = forms.DateField(label="To", widget=widgets.DatePickerWidget())
@@ -13,11 +14,16 @@ class ViewReportForm(forms.Form):
 class FindStudentForm(forms.Form):
     first_name = forms.CharField(max_length=200, required=False)
     last_name = forms.CharField(max_length=200, required=False)
-    ssn = forms.CharField(max_length=11, required=False)
-    email = forms.EmailField(required=False)
+    ssn = forms.CharField(max_length=11, required=False, widget=widgets.InputMaskWidget(input_mask={'alias': 'ssn'}))
+    email = forms.EmailField(required=False, widget=widgets.InputMaskWidget(input_mask={'alias': 'email'}))
     home_phone = PhoneNumberField(required=False)
     cell_phone = PhoneNumberField(required=False)
-    zipcode = forms.CharField(max_length=20, required=False)
+    zipcode = forms.CharField(max_length=20, required=False,
+                              widget=widgets.InputMaskWidget(input_mask={'mask': '99999'}))
+
+    def clean_ssn(self):
+        ssn = self.cleaned_data.get('ssn')
+        return ssn.replace('-', '')
 
 
 class ModifyContractLookupForm(forms.Form):
--- wiawdp/widgets.py
+++ wiawdp/widgets.py
@@ -8,7 +8,7 @@ class DatePickerWidget(forms.DateInput):
         return context
 
     class Media:
-        js = ('js/jquery-3.5.1.min.js', 'AdminLTE-3.0.5/plugins/moment/moment.min.js',
+        js = ('AdminLTE-3.0.5/plugins/jquery/jquery.min.js', 'AdminLTE-3.0.5/plugins/moment/moment.min.js',
               'AdminLTE-3.0.5/plugins/daterangepicker/daterangepicker.js', 'js/widgets/datepickerwidget.js')
 
 
@@ -16,5 +16,23 @@ class DateTimePickerWidget(forms.DateInput):
     template_name = 'wiawdp/widgets/datetimepickerwidget.html'
 
     class Media:
-        js = ('js/jquery-3.5.1.min.js', 'AdminLTE-3.0.5/plugins/moment/moment.min.js',
+        js = ('AdminLTE-3.0.5/plugins/jquery/jquery.min.js', 'AdminLTE-3.0.5/plugins/moment/moment.min.js',
               'AdminLTE-3.0.5/plugins/daterangepicker/daterangepicker.js', 'js/widgets/datetimepickerwidget.js')
+
+
+class InputMaskWidget(forms.TextInput):
+    def __init__(self, attrs=None, input_mask={}):
+        self.input_mask = input_mask
+        super().__init__(attrs)
+
+    def mask_dict_to_string(self):
+        pairs = (f"'{key}': '{value}'" for key, value in self.input_mask.items())
+        return ','.join(pairs)
+
+    def get_context(self, name, value, attrs):
+        attrs['data-inputmask'] = self.mask_dict_to_string()
+        attrs['data-custom-widget'] = self.__class__.__name__
+        return super().get_context(name, value, attrs)
+
+    class Media:
+        js = ('AdminLTE-3.0.5/plugins/inputmask/inputmask.min.js', 'js/widgets/inputmaskwidget.js')
