bf70b1a6cca8c7a1a32170b8cfeebf02e2988d72
Calvin Ling <calvinling8@gmail.com>
2020-07-25 11:58:08 -0400

    Use test fixtures

--- wiawdp/test_views.py
+++ wiawdp/test_views.py
@@ -6,40 +6,14 @@ from django.utils import timezone, dateformat
 from students.models import Student
 from wiawdp.models import Workforce, Contract
 from wiawdp.views import AddContractView
-
-
-def create_user(username, email, password, is_active=True, is_staff=False, is_superuser=False):
-    return User.objects.create_user(username=username, email=email, password=password, is_active=is_active,
-                               is_staff=is_staff,
-                               is_superuser=is_superuser)
-
-
-def create_admin():
-    return create_user('admin', '', '', True, True, True)
-
-
-def create_student(id, first_name, last_name, ssn, zip_code, city, cell_phone, email, location, refer, sources):
-    return Student.objects.create(id=id, first_name=first_name, last_name=last_name, ssn=ssn, zipcode=zip_code,
-                                  city=city,
-                                  cellPhone=cell_phone, email=email, location=location, refer=refer, sources=sources)
-
-
-def create_workforce(workforce):
-    return Workforce.objects.create(workforce=workforce)
-
-
-def create_contract(client, workforce, end_date, performance):
-    return Contract.objects.create(client=client, workforce=workforce, end_date=end_date, performance=performance)
-
-
-def create_and_login_as_admin(test_case):
-    test_case.admin = create_admin()
-    test_case.client.force_login(test_case.admin)
+from wiawdp.tests import login_as_admin
 
 
 class ContractViewTestCase(TestCase):
+    fixtures = ['user.json']
+
     def setUp(self):
-        create_and_login_as_admin(self)
+        login_as_admin(self)
 
     def test_default_redirect(self):
         response = self.client.get(reverse('wiawdp:contracts'))
@@ -59,12 +33,12 @@ class ContractViewTestCase(TestCase):
 
 
 class AddContractViewTestCase(TestCase):
+    fixtures = ['student.json', 'user.json', 'workforce.json']
+
     def setUp(self):
-        self.admin = create_admin()
-        self.client.force_login(self.admin)
-        self.student = create_student(0, 'Name', 'Surname', '000000000', '00000', 'City', '+18888888888',
-                                      'someone@example.com', 'Location', 'refer', '')
-        self.workforce = create_workforce('work force')
+        login_as_admin(self)
+        self.student = Student.objects.get(pk=1)
+        self.workforce = Workforce.objects.get(pk=1)
 
     def test_post_filled_form(self):
         data = {
@@ -79,9 +53,10 @@ class AddContractViewTestCase(TestCase):
 
 
 class SearchContractsViewTestCase(TestCase):
+    fixtures = ['user.json']
+
     def setUp(self):
-        self.admin = create_admin()
-        self.client.force_login(self.admin)
+        login_as_admin(self)
 
     def test_post_filled_form(self):
         data = {
@@ -99,20 +74,20 @@ class SearchContractsViewTestCase(TestCase):
 
 
 class ModifyContractViewTestCase(TestCase):
+    fixtures = ['contract.json', 'student.json', 'user.json', 'workforce.json']
+
     def setUp(self):
-        self.admin = create_admin()
-        self.client.force_login(self.admin)
-        self.student = create_student(0, 'Name', 'Surname', '000000000', '00000', 'City', '+18888888888',
-                                      'someone@example.com', 'Location', 'refer', '')
+        login_as_admin(self)
+        self.student = Student.objects.get(pk=1)
         self.end_date = timezone.now()
-        self.workforce = create_workforce('Red')
-        self.performance = '5'
-        self.contract = create_contract(self.student, self.workforce, self.end_date, self.performance)
+        self.workforce = Workforce.objects.get(pk=1)
+        self.performance = 5
+        self.contract = Contract.objects.get(pk=1)
 
     def test_post_filled_form(self):
         updated_end_date = self.contract.end_date + timezone.timedelta(days=1)
         updated_end_date_str = dateformat.format(updated_end_date, 'Y-m-d H:i:s')
-        updated_workforce = create_workforce('Green')
+        updated_workforce = Workforce.objects.get(pk=2)
         updated_performance = 3
 
         data = {
@@ -130,9 +105,10 @@ class ModifyContractViewTestCase(TestCase):
 
 
 class ModifyContractLookupViewTestCase(TestCase):
+    fixtures = ['user.json']
+
     def setUp(self):
-        self.admin = create_admin()
-        self.client.force_login(self.admin)
+        login_as_admin(self)
 
     def test_post_filled_form(self):
         response = self.client.post(reverse('wiawdp:modify_contract_lookup'), {'student_id': 0})
@@ -141,53 +117,36 @@ class ModifyContractLookupViewTestCase(TestCase):
 
 
 class DeleteContractViewTestCase(TestCase):
-    def setUp(self):
-        self.admin = create_user('admin', 'admin@avtech.com', 'avtech123', True, True, True)
-        self.student = create_student(0, 'John', 'Doe', '888888888', '88888', 'City', '8888888888', 'email@email.com',
-                                      'ABC Town',
-                                      'ref', 'source')
-        create_workforce('Red')
-        create_workforce('Green')
-        create_workforce('Blue')
+    fixtures = ['contract.json', 'student.json', 'user.json', 'workforce.json']
 
-        self.client.force_login(self.admin)
+    def setUp(self):
+        login_as_admin(self)
+        self.student = Student.objects.get(pk=1)
+        self.initial_num_contracts = Contract.objects.count()
 
     def test_delete_none(self):
-        workforce = Workforce.objects.get(workforce='Red')
-        create_contract(self.student, workforce, timezone.now(), 1)
-
+        num_contracts = Contract.objects.count()
         self.client.post(reverse('wiawdp:delete_contracts'), {'row_pks': []})
-        self.assertEqual(Contract.objects.count(), 1)
+        self.assertEqual(Contract.objects.count(), num_contracts)
 
     def test_delete_single(self):
-        workforce = Workforce.objects.get(workforce='Red')
-        c_1 = create_contract(self.student, workforce, timezone.now(), 1)
-        create_contract(self.student, workforce, timezone.now(), 2)
-        create_contract(self.student, workforce, timezone.now(), 3)
-        create_contract(self.student, workforce, timezone.now(), 3)
-        create_contract(self.student, workforce, timezone.now(), 5)
+        c_1 = Contract.objects.get(pk=1)
 
         self.client.post(reverse('wiawdp:delete_contracts'), {'row_pks': [c_1.pk]})
         self.assertRaises(Contract.DoesNotExist, Contract.objects.get, pk=c_1.pk)
-        self.assertEqual(Contract.objects.count(), 4)
+        self.assertEqual(Contract.objects.count(), self.initial_num_contracts - 1)
 
     def test_delete_multiple(self):
-        workforce = Workforce.objects.get(workforce='Red')
-        c_1 = create_contract(self.student, workforce, timezone.now(), 1)
-        c_2 = create_contract(self.student, workforce, timezone.now(), 2)
-        c_3 = create_contract(self.student, workforce, timezone.now(), 3)
-        create_contract(self.student, workforce, timezone.now(), 3)
-        create_contract(self.student, workforce, timezone.now(), 5)
+        c_1 = Contract.objects.get(pk=1)
+        c_2 = Contract.objects.get(pk=2)
+        c_3 = Contract.objects.get(pk=3)
 
         self.client.post(reverse('wiawdp:delete_contracts'), {'row_pks': [c_1.pk, c_2.pk, c_3.pk]})
         self.assertRaises(Contract.DoesNotExist, Contract.objects.get, pk=c_1.pk)
         self.assertRaises(Contract.DoesNotExist, Contract.objects.get, pk=c_2.pk)
         self.assertRaises(Contract.DoesNotExist, Contract.objects.get, pk=c_3.pk)
-        self.assertEqual(Contract.objects.count(), 2)
+        self.assertEqual(Contract.objects.count(), self.initial_num_contracts - 3)
 
     def test_delete_nonexistant(self):
-        workforce = Workforce.objects.get(workforce='Red')
-        create_contract(self.student, workforce, timezone.now(), 1)
-
         self.client.post(reverse('wiawdp:delete_contracts'), {'row_pks': [100]})
-        self.assertEqual(Contract.objects.count(), 1)
+        self.assertEqual(Contract.objects.count(), self.initial_num_contracts)
--- wiawdp/tests.py
+++ wiawdp/tests.py
@@ -1,1 +1,16 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+
+from wiawdp.models import Workforce, Contract
+
+
+def create_workforce(workforce):
+    return Workforce.objects.create(workforce=workforce)
+
+
+def create_contract(client, workforce, end_date, performance):
+    return Contract.objects.create(client=client, workforce=workforce, end_date=end_date, performance=performance)
+
+
+def login_as_admin(test_case):
+    test_case.admin = User.objects.get(username='admin')
+    test_case.client.force_login(test_case.admin)
